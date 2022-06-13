01b70e43e95e6c695b35ee9248b736a8a407b2a2
christine-nkatha <nkathachristine456@gmail.com>
2022-06-13 14:59:17 +0300

    debuged an importation error

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -51,7 +51,10 @@ INSTALLED_APPS = [
 
     #own installed
     'awwwardapp',
-    'bootstrap4'
+    'bootstrap4',
+    'rest_framework',
+    'crispy_forms',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +72,10 @@ ROOT_URLCONF = 'awwwards.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,10 +142,26 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#...
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 django_heroku.settings(locals())
