80cd5f19eb383c479f1218efdc518bf2a0f7d6d5
christine-nkatha <nkathachristine456@gmail.com>
2022-06-13 17:13:46 +0300

    fixed a bug on the models

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+$(document).on("scroll", function () {
+    if ($(document).scrollTop() > 50) {
+      $("#nav").addClass("shrink");
+    } else {
+      $("#nav").removeClass("shrink");
+    }
+  });
+  
