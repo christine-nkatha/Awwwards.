df09e83117194cc0ba2bba0dcec1b699e28f0768
christine-nkatha <nkathachristine456@gmail.com>
2022-06-16 14:49:27 +0300

    url changes

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     #path('accounts/register/', RegistrationView.as_view(success_url='/home'),name='django_registration_register'),
    #path('accounts/', include('django.contrib.auth.urls')),
    # path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
-    #path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
     
 ]
